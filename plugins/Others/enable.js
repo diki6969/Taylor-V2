ce1ef6b788bfc4ba11394188667018ca07cd29fb
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-12-05 13:22:22 +0800

    Add files via upload

--- plugins/Others/enable.js
+++ plugins/Others/enable.js
@@ -1,4 +1,4 @@
-let handler = async (m, {
+const handler = async (m, {
     conn,
     usedPrefix,
     command,
@@ -7,67 +7,40 @@ let handler = async (m, {
     isAdmin,
     isROwner
 }) => {
-    const features = ["antiBot", "antiFoto", "antiVideo", "antiAudio", "antiCall", "antiDelete", "antiLink", "antiLinkFb", "antiLinkHttp", "antiLinkIg", "antiLinkTel", "antiLinkTik", "antiLinkWa", "antiLinkYt", "antiSatir", "antiSticker", "antiVirtex", "antiToxic", "antibule", "autoBio", "autoChat", "autoAi", "autoGpt", "autochatGpt", "autoJoin", "autoPresence", "autoReply", "autoSticker", "autoVn", "viewStory", "bcjoin", "detect", "getmsg", "nsfw", "antiSpam", "simi", "alicia", "gptvoice", "characterai", "updateAnime", "updateAnimeNews", "viewonce", "welcome", "autoread", "gconly", "nyimak", "pconly", "self", "swonly", "lastAnime", "latestNews"];
+    const features = ["antiBot", "antiFoto", "antiVideo", "antiAudio", "antiCall", "antiDelete", "antiLink", "antiLinkFb", "antiLinkHttp", "antiLinkIg", "antiLinkTel", "antiLinkTik", "antiLinkWa", "antiLinkYt", "antiSatir", "antiSticker", "antiVirtex", "antiToxic", "antibule", "autoBio", "autoChat", "autoAi", "autoGpt", "autochatGpt", "autoJoin", "autoPresence", "autoReply", "autoSticker", "autoVn", "viewStory", "bcjoin", "detect", "getmsg", "nsfw", "antiSpam", "simi", "alicia", "gptvoice", "characterai", "updateAnime", "updateAnimeNews", "viewonce", "welcome", "autoread", "gconly", "nyimak", "pconly", "self", "rpg", "swonly", "lastAnime", "latestNews"];
     const activeFeatures = ["antiDelete", "detect", "getmsg", "lastAnime", "latestNews", "welcome"];
+    
     const result = features.map((f, i) => {
         const isActive = activeFeatures.includes(f) ? !global.db.data.chats[m.chat][f] : global.db.data.chats[m.chat][f];
-        return `*${(i + 1).toString().padEnd(2)}.* ${f.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()).padEnd(18)} *${isActive ? "ON" : "OFF"}*`;
-    }).join('\n');
+        return `${(i + 1).toString().padEnd(2)}. ${f.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()).padEnd(18)} ${isActive ? "ON" : "OFF"}`;
+    }).join('\n').trimEnd();
 
-    const featureStatus = `*# Feature*            *Mode*\n${"-".repeat(33)}\n${result}`;
-    const listEnab = `🛠️ *DAFTAR FITUR*
+    const isEnable = !/false|disable|(turn)?off|0/i.test(command);
+    const chat = global.db.data.chats[m.chat];
 
-${featureStatus}
-
-*📝 CARA MENGGUNAKAN:*
-→ ${usedPrefix + command} [nomor atau nama fitur]`;
-
-    let isEnable = !/false|disable|(turn)?off|0/i.test(command);
-    let chat = global.db.data.chats[m.chat];
-    let user = global.db.data.users[m.sender];
-
-    // Ambil input dari args
-    let input = args[0];
-
-    // Cek apakah input adalah angka
-    let isNumber = !isNaN(input);
-
-    let featureName;
-
-    if (isNumber) {
-        let index = parseInt(input) - 1;
-
-        if (index < 0 || index >= features.length) {
-            return await conn.reply(m.chat, listEnab, m);
-        }
-
-        featureName = features[index];
-    } else {
-        // Jika input bukan angka, gunakan itu sebagai nama fitur
-        featureName = input;
-    }
-
-    // Periksa apakah featureName cocok dengan salah satu elemen dalam daftar fitur
-    if (!features.includes(featureName)) {
-        return await conn.reply(m.chat, listEnab, m);
-    }
-
-    // ...
+    const input = args[0];
+    const isNumber = !isNaN(input);
+    const featureName = isNumber ? features[parseInt(input) - 1] : input;
+    const responseText = "```" + `${result}\n` + "```";
+    const featureStatus = `*# Feature*            *Mode*\n${"-".repeat(33)}\n${responseText}`;
+    const listEnab = `🛠️ *DAFTAR FITUR*\n${featureStatus}\n*📝 CARA MENGGUNAKAN:*\n→ ${usedPrefix + command} [nomor atau nama fitur]`.trimStart();
+    
+    if (!features.includes(featureName)) return await conn.reply(m.chat, listEnab, m, adReplyS);
 
     if (activeFeatures.includes(featureName)) {
         chat[featureName] = !isEnable;
-        conn.reply(m.chat, `Feature *${featureName.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())}* mode *${isEnable ? 'ON' : 'OFF'}*`, m);
     } else {
         if (["autoChat"].includes(featureName)) {
-            conn.autochat = conn.autochat ? conn.autochat : {}
+            conn.autochat = conn.autochat || {};
             conn.autochat.status = isEnable;
-            conn.reply(m.chat, `Feature *${featureName.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())}* mode *${isEnable ? 'ON' : 'OFF'}*`, m);
         } else {
             chat[featureName] = isEnable;
-            conn.reply(m.chat, `Feature *${featureName.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())}* mode *${isEnable ? 'ON' : 'OFF'}*`, m);
         }
     }
+
+    conn.reply(m.chat, `Feature *${featureName.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())}* mode *${isEnable ? 'ON' : 'OFF'}*`, m);
 }
+
 handler.help = ["en", "dis"].map(v => v + "able <nomor atau nama fitur>");
 handler.tags = ["group", "owner"];
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i;

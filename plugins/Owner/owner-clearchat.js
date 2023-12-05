c3689c7d58e66c27fab12222ebd94401bbd73b4e
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-12-05 13:21:33 +0800

    Add files via upload

--- plugins/Owner/owner-clearchat.js
+++ plugins/Owner/owner-clearchat.js
@@ -1,23 +1,46 @@
 let handler = async (m, {
-    conn
+	conn
 }) => {
-    try {
-        const chatIdsToDelete = Object.values(conn.chats).filter(item => /@g\.us$/.test(item.id)).map(item => item.id);
-        const deletedGroupCount = chatIdsToDelete.length;
-        for (const id of chatIdsToDelete) {
-            await conn.chatModify({
-                delete: true,
-                lastMessages: [{
-                    key: m.key,
-                    messageTimestamp: m.messageTimestamp
-                }]
-            }, id);
-        }
-        await conn.reply(m.chat, `*Jumlah grup:* ${deletedGroupCount}`, m);
-    } catch (error) {
-        console.error(error);
-        await conn.reply(m.chat, 'Terjadi kesalahan dalam menghapus grup.', m);
-    }
+	try {
+		const chatIdsToDeleteGroup = Object.values(conn.chats)
+			.filter(item => /@g\.us$/.test(item.id))
+			.map(item => item.id);
+
+		const deletedGroupCount = chatIdsToDeleteGroup.length;
+
+		for (const id of chatIdsToDeleteGroup) {
+			await conn.chatModify({
+				delete: true,
+				lastMessages: [{
+					key: m.key,
+					messageTimestamp: m.messageTimestamp
+				}]
+			}, id);
+		}
+
+		const chatIdsToDeletePrivate = Object.values(conn.chats)
+			.filter(item => /@s.whatsapp\.net$/.test(item.id))
+			.map(item => item.id);
+
+		const deletedPrivateCount = chatIdsToDeletePrivate.length;
+
+		for (const id of chatIdsToDeletePrivate) {
+			await conn.chatModify({
+				delete: true,
+				lastMessages: [{
+					key: m.key,
+					messageTimestamp: m.messageTimestamp
+				}]
+			}, id);
+		}
+
+		const combinedResult = `🗑️ *Deleted Group Chats:* ${deletedGroupCount}\n🗑️ *Deleted Private Chats:* ${deletedPrivateCount}`;
+		await conn.reply(m.chat, combinedResult, m);
+
+	} catch (error) {
+		console.error(error);
+		await conn.reply(m.chat, 'Terjadi kesalahan dalam menghapus grup.', m);
+	}
 }
 handler.help = ['clearchat']
 handler.tags = ['owner']

6e21f11a0be82b18a4198269d8a2457437ddb089
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-12-12 15:45:44 +0800

    Add files via upload

--- plugins/AI/ai-bingimg.js
+++ plugins/AI/ai-bingimg.js
@@ -1,51 +1,51 @@
-const { BingImageCreator } = await(await import('../../lib/ai/bing-image.js'));
+const { BingImageCreator } = await (await import('../../lib/ai/bing-image.js'));
 
-const handler = async (m, {
-	conn,
-	args,
-	usedPrefix,
-	command
-}) => {
-	let text;
-	if (args.length >= 1) {
-		text = args.slice(0).join(" ");
-	} else if (m.quoted && m.quoted.text) {
-		text = m.quoted.text;
-	} else {
-		throw 'Input teks atau reply teks!';
-	}
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+    let text;
 
-	await m.reply(wait);
-	try {
-		const res = new BingImageCreator({ cookie: "1CBrGSpML0Fz8WQSDRzqWaeyL9zle6nYrZn6uCwVyEEO8Nqdcs4B2UGs-zBkYVeTjYmvveLcSvkWvDtPHVV8CtUt0l15dzoSU_ARtKpYzDes8WjEKQPjWX64ckraHm676gEcRMa2dVE_nGuCLpFvnkDBdzkO_Kfesi4LgVMDrucBRmOPrSOVYzqPJVFXtNIOLDlW5xOUUi3rS8ltxZfSoCQ" });
-		const data = await res.createImage(text);
-		if (data.length > 0) {
-			for (let i = 0; i < data.length; i++) {
-				try {
-					if (!data[i].endsWith('.svg')) {
-						await conn.sendFile(
-							m.chat,
-							data[i],
-							'',
-							`Image *(${i + 1}/${data.length - 1})*`,
-							m,
-							false, {
-								mentions: [m.sender],
-							}
-						);
-					}
-				} catch (error) {
-					console.error(`Error sending file: ${error.message}`);
-					await m.reply(`Failed to send image *(${i + 1}/${data.length})*`);
-				}
-			}
-		} else {
-			await m.reply('No images found.');
-		}
-	} catch (error) {
-		console.error(`Error in handler: ${error.message}`);
-		await m.reply('An error occurred while processing the request.');
-	}
+    if (args.length >= 1) {
+        text = args.join(" ");
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text;
+    } else {
+        throw 'Input teks atau reply teks!';
+    }
+
+    await m.reply(wait);
+
+    try {
+        const res = new BingImageCreator({ cookie: "1CBrGSpML0Fz8WQSDRzqWaeyL9zle6nYrZn6uCwVyEEO8Nqdcs4B2UGs-zBkYVeTjYmvveLcSvkWvDtPHVV8CtUt0l15dzoSU_ARtKpYzDes8WjEKQPjWX64ckraHm676gEcRMa2dVE_nGuCLpFvnkDBdzkO_Kfesi4LgVMDrucBRmOPrSOVYzqPJVFXtNIOLDlW5xOUUi3rS8ltxZfSoCQ" });
+        const data = await res.createImage(text);
+
+        const filteredData = data.filter(file => !file.endsWith('.svg'));
+        const totalCount = filteredData.length;
+
+        if (totalCount > 0) {
+            for (let i = 0; i < totalCount; i++) {
+                try {
+                    await conn.sendFile(
+                        m.chat,
+                        filteredData[i],
+                        '',
+                        `Image *(${i + 1}/${totalCount})*`,
+                        m,
+                        false,
+                        {
+                            mentions: [m.sender],
+                        }
+                    );
+                } catch (error) {
+                    console.error(`Error sending file: ${error.message}`);
+                    await m.reply(`Failed to send image *(${i + 1}/${totalCount})*`);
+                }
+            }
+        } else {
+            await m.reply('No images found after filtering.');
+        }
+    } catch (error) {
+        console.error(`Error in handler: ${error.message}`);
+        await m.reply('An error occurred while processing the request.');
+    }
 };
 
 handler.help = ["bingimg *[query]*"];
